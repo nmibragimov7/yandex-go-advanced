cbff7d5024b9fa8d4cb47b496e7bde849f5b0fa7
NIbragimov <nibragimov@1cb.kz>
2024-11-24 10:31:05 +0500

    feat: handler test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,96 +1,155 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"math"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 // User — пользователь в системе.
-type User struct {
-	FirstName string
-	LastName  string
-}
+//type User struct {
+//	FirstName string
+//	LastName  string
+//}
 
 // FullName возвращает имя и фамилию пользователя.
-func (u User) FullName() string {
-	return u.FirstName + " " + u.LastName
-}
+//func (u User) FullName() string {
+//	return u.FirstName + " " + u.LastName
+//}
 
 // Relationship определяет положение в семье.
-type Relationship string
+//type Relationship string
 
 // Возможные роли в семье.
-const (
-	Father      = Relationship("father")
-	Mother      = Relationship("mother")
-	Child       = Relationship("child")
-	GrandMother = Relationship("grandMother")
-	GrandFather = Relationship("grandFather")
-)
+//const (
+//	Father      = Relationship("father")
+//	Mother      = Relationship("mother")
+//	Child       = Relationship("child")
+//	GrandMother = Relationship("grandMother")
+//	GrandFather = Relationship("grandFather")
+//)
 
 // Family описывает семью.
-type Family struct {
-	Members map[Relationship]Person
-}
+//type Family struct {
+//	Members map[Relationship]Person
+//}
 
 // Person описывает конкретного человека в семье.
-type Person struct {
-	FirstName string
-	LastName  string
-	Age       int
-}
+//type Person struct {
+//	FirstName string
+//	LastName  string
+//	Age       int
+//}
 
-var (
-	// ErrRelationshipAlreadyExists возвращает ошибку, если роль уже занята.
-	ErrRelationshipAlreadyExists = errors.New("relationship already exists")
-)
+//var (
+//	// ErrRelationshipAlreadyExists возвращает ошибку, если роль уже занята.
+//	ErrRelationshipAlreadyExists = errors.New("relationship already exists")
+//)
 
 // AddNew добавляет нового члена семьи.
 // Если в семье ещё нет людей, создаётся пустая мапа.
 // Если роль уже занята, метод выдаёт ошибку.
-func (f *Family) AddNew(r Relationship, p Person) error {
-	if f.Members == nil {
-		f.Members = map[Relationship]Person{}
+//func (f *Family) AddNew(r Relationship, p Person) error {
+//	if f.Members == nil {
+//		f.Members = map[Relationship]Person{}
+//	}
+//	if _, ok := f.Members[r]; ok {
+//		return ErrRelationshipAlreadyExists
+//	}
+//	f.Members[r] = p
+//	return nil
+//}
+
+// User — основной объект для теста.
+type User struct {
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+// UserViewHandler — хендлер, который нужно протестировать.
+func UserViewHandler(users map[string]User) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+
+		userId := r.URL.Query().Get("user_id")
+		if userId == "" {
+			//http.Error(rw, "user_id is empty", http.StatusBadRequest)
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte(`{"message": "user_id is empty"}`))
+			return
+		}
+
+		user, ok := users[userId]
+		if !ok {
+			//http.Error(rw, "user not found", http.StatusNotFound)
+			rw.WriteHeader(http.StatusNotFound)
+			rw.Write([]byte(`{"message": "user not found"}`))
+			return
+		}
+
+		jsonUser, err := json.Marshal(user)
+		if err != nil {
+			//http.Error(rw, "can't provide a json. internal error",
+			//	http.StatusInternalServerError)
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte(`{"message": "can't provide a json. internal error"}`))
+			return
+		}
+
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(jsonUser)
 	}
-	if _, ok := f.Members[r]; ok {
-		return ErrRelationshipAlreadyExists
-	}
-	f.Members[r] = p
-	return nil
 }
 
 func main() {
 	// 1 fragment
-	v := Abs(3)
-	fmt.Println(v)
+	//v := Abs(3)
+	//fmt.Println(v)
 
 	// 2 fragment
-	u := User{
+	//u := User{
+	//	FirstName: "Misha",
+	//	LastName:  "Popov",
+	//}
+	//fmt.Println(u.FullName())
+
+	// 3 fragment
+	//f := Family{}
+	//err := f.AddNew(Father, Person{
+	//	FirstName: "Misha",
+	//	LastName:  "Popov",
+	//	Age:       56,
+	//})
+	//fmt.Println(f, err)
+
+	//err = f.AddNew(Father, Person{
+	//	FirstName: "Drug",
+	//	LastName:  "Mishi",
+	//	Age:       57,
+	//})
+	//fmt.Println(f, err)
+
+	users := make(map[string]User)
+	u1 := User{
+		ID:        "u1",
 		FirstName: "Misha",
 		LastName:  "Popov",
 	}
-	fmt.Println(u.FullName())
-
-	// 3 fragment
-	f := Family{}
-	err := f.AddNew(Father, Person{
-		FirstName: "Misha",
+	u2 := User{
+		ID:        "u2",
+		FirstName: "Sasha",
 		LastName:  "Popov",
-		Age:       56,
-	})
-	fmt.Println(f, err)
+	}
+	users["u1"] = u1
+	users["u2"] = u2
 
-	err = f.AddNew(Father, Person{
-		FirstName: "Drug",
-		LastName:  "Mishi",
-		Age:       57,
-	})
-	fmt.Println(f, err)
+	http.HandleFunc("/users", UserViewHandler(users))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // Abs возвращает абсолютное значение.
 // Например: 3.1 => 3.1, -3.14 => 3.14, -0 => 0.
-func Abs(value float64) float64 {
-	return math.Abs(value)
-}
+//func Abs(value float64) float64 {
+//	return math.Abs(value)
+//}
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -2,113 +2,194 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestAbs(t *testing.T) {
+//func TestAbs(t *testing.T) {
+//	tests := []struct {
+//		name   string
+//		values float64
+//		want   float64
+//	}{
+//		{
+//			name:   "-3 to 3",
+//			values: -3,
+//			want:   3,
+//		},
+//		{
+//			name:   "-2.000001 to 2.000001",
+//			values: -2.000001,
+//			want:   2.000001,
+//		},
+//		{
+//			name:   "-0.000000003 to 0.000000003",
+//			values: -0.000000003,
+//			want:   0.000000003,
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			assert.Equal(t, test.want, Abs(test.values))
+//			//if res := Abs(test.values); res != test.want {
+//			//	t.Errorf("Abs() = %f, want %f", res, test.want)
+//			//}
+//		})
+//	}
+//}
+//
+//func TestUser_FullName(t *testing.T) {
+//	tests := []struct {
+//		name   string
+//		values User
+//		want   string
+//	}{
+//		{
+//			name: "1 test",
+//			values: User{
+//				FirstName: "Nurlan",
+//				LastName:  "Ibragimov",
+//			},
+//			want: "Nurlan Ibragimov",
+//		},
+//		{
+//			name: "2 test",
+//			values: User{
+//				FirstName: "Petr",
+//				LastName:  "Petrov",
+//			},
+//			want: "Petr Petrov",
+//		},
+//	}
+//
+//	for _, test := range tests { // цикл по всем тестам
+//		t.Run(test.name, func(t *testing.T) {
+//			assert.Equal(t, test.want, test.values.FullName())
+//			//if res := test.values.FullName(); res != test.want {
+//			//	t.Errorf("user.FullName() = %s, want %s", res, test.want)
+//			//}
+//		})
+//	}
+//}
+//
+//func TestFamily_AddNew(t *testing.T) {
+//	f := Family{}
+//
+//	tests := []struct {
+//		name         string
+//		relationship Relationship
+//		values       Person
+//		want         bool
+//	}{
+//		{
+//			name:         "add father",
+//			relationship: Father,
+//			values: Person{
+//				FirstName: "Nurlan",
+//				LastName:  "Ibragimov",
+//				Age:       31,
+//			},
+//			want: false,
+//		},
+//		{
+//			name:         "double add father",
+//			relationship: Father,
+//			values: Person{
+//				FirstName: "Petr",
+//				LastName:  "Petrov",
+//				Age:       31,
+//			},
+//			want: true,
+//		},
+//	}
+//
+//	for _, test := range tests { // цикл по всем тестам
+//		t.Run(test.name, func(t *testing.T) {
+//			assert.Equal(t, test.want, f.AddNew(test.relationship, test.values) != nil)
+//			//if res := f.AddNew(test.relationship, test.values); (res != nil) != test.want {
+//			//	t.Errorf("f.AddNew() = %s, want %t", res, test.want)
+//			//}
+//		})
+//	}
+//}
+
+func TestUserViewHandler(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
 	tests := []struct {
-		name   string
-		values float64
-		want   float64
+		name string
+		url  string
+		want want
 	}{
 		{
-			name:   "-3 to 3",
-			values: -3,
-			want:   3,
+			name: "negative test #1",
+			url:  "/users",
+			want: want{
+				code:        400,
+				response:    `{"message": "user_id is empty"}`,
+				contentType: "application/json",
+			},
 		},
 		{
-			name:   "-2.000001 to 2.000001",
-			values: -2.000001,
-			want:   2.000001,
+			name: "negative test #2",
+			url:  "/users?user_id=u3",
+			want: want{
+				code:        404,
+				response:    `{"message": "user not found"}`,
+				contentType: "application/json",
+			},
 		},
 		{
-			name:   "-0.000000003 to 0.000000003",
-			values: -0.000000003,
-			want:   0.000000003,
+			name: "positive test #3",
+			url:  "/users?user_id=u1",
+			want: want{
+				code:        200,
+				response:    `{"ID":"u1","FirstName":"Misha","LastName":"Popov"}`,
+				contentType: "application/json",
+			},
 		},
 	}
+
+	users := make(map[string]User)
+	u1 := User{
+		ID:        "u1",
+		FirstName: "Misha",
+		LastName:  "Popov",
+	}
+	u2 := User{
+		ID:        "u2",
+		FirstName: "Sasha",
+		LastName:  "Popov",
+	}
+	users["u1"] = u1
+	users["u2"] = u2
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.want, Abs(test.values))
-			//if res := Abs(test.values); res != test.want {
-			//	t.Errorf("Abs() = %f, want %f", res, test.want)
-			//}
-		})
-	}
-}
+			request := httptest.NewRequest(http.MethodGet, test.url, nil)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			handler := UserViewHandler(users)
+			handler.ServeHTTP(w, request)
 
-func TestUser_FullName(t *testing.T) {
-	tests := []struct {
-		name   string
-		values User
-		want   string
-	}{
-		{
-			name: "1 test",
-			values: User{
-				FirstName: "Nurlan",
-				LastName:  "Ibragimov",
-			},
-			want: "Nurlan Ibragimov",
-		},
-		{
-			name: "2 test",
-			values: User{
-				FirstName: "Petr",
-				LastName:  "Petrov",
-			},
-			want: "Petr Petrov",
-		},
-	}
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, test.want.code, res.StatusCode)
 
-	for _, test := range tests { // цикл по всем тестам
-		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.want, test.values.FullName())
-			//if res := test.values.FullName(); res != test.want {
-			//	t.Errorf("user.FullName() = %s, want %s", res, test.want)
-			//}
-		})
-	}
-}
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
 
-func TestFamily_AddNew(t *testing.T) {
-	f := Family{}
-
-	tests := []struct {
-		name         string
-		relationship Relationship
-		values       Person
-		want         bool
-	}{
-		{
-			name:         "add father",
-			relationship: Father,
-			values: Person{
-				FirstName: "Nurlan",
-				LastName:  "Ibragimov",
-				Age:       31,
-			},
-			want: false,
-		},
-		{
-			name:         "double add father",
-			relationship: Father,
-			values: Person{
-				FirstName: "Petr",
-				LastName:  "Petrov",
-				Age:       31,
-			},
-			want: true,
-		},
-	}
-
-	for _, test := range tests { // цикл по всем тестам
-		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.want, f.AddNew(test.relationship, test.values) != nil)
-			//if res := f.AddNew(test.relationship, test.values); (res != nil) != test.want {
-			//	t.Errorf("f.AddNew() = %s, want %t", res, test.want)
-			//}
+			require.NoError(t, err)
+			assert.JSONEq(t, test.want.response, string(resBody))
+			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
 		})
 	}
 }
