9bc78fc27b51ee756455924bdf3c948f44e7d7e0
NIbragimov <nibragimov@1cb.kz>
2024-12-17 17:42:27 +0500

    feat: add logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"yandex-go-advanced/internal/config"
 	"yandex-go-advanced/internal/handlers"
+	"yandex-go-advanced/internal/logger"
 	"yandex-go-advanced/internal/storage"
 )
 
 func main() {
 	cnf := config.Init().GetConfig()
 	str := storage.NewStore()
+	sgr := logger.InitLogger()
 
-	log.Fatal(http.ListenAndServe(*cnf.Server, handlers.Router(cnf, str)))
+	log.Fatal(http.ListenAndServe(*cnf.Server, handlers.Router(cnf, str, sgr)))
 }
